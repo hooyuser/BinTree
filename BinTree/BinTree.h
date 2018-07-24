7f0e0883c6f0c2c6b0106713bae84d0a34639c14
hooyuser <hooyuser@outlook.com>
2018-07-24 23:59:33 +0800

    update cpp.hint

--- BinTree/BinTree.h
+++ BinTree/BinTree.h
@@ -5,7 +5,6 @@
 #include <algorithm>
 #include <typeinfo.h>
 #include "stack.h"
-//#include "vector.h"
 #include "queue.h"
 
 
@@ -68,7 +67,10 @@ struct Cleaner<T*>
 {
 	static void clean(T* x)
 	{
-		if (x) { delete x; } //如果其中包含指针，递归释放
+		if (x)
+		{
+			delete x;  //如果其中包含指针，递归释放
+		}
 #ifdef _DEBUG                
 		static int n = 0;
 		printf("\t<%s>[%d] released\n", typeid (T*).name(), ++n);
@@ -234,7 +236,8 @@ void travPre_I2(BinNodePosi(T) x, VST& visit)  //二叉树先序遍历算法（迭代版#2）
 	while (true)
 	{
 		visitAlongVine(x, visit, S); //从当前节点出发，逐批访问
-		if (S.empty()) break; //若栈空，则退出循环
+		if (S.empty())
+			break; //若栈空，则退出循环
 		x = S.pop(); //弹出下一批的起点
 	}
 }
@@ -242,7 +245,8 @@ void travPre_I2(BinNodePosi(T) x, VST& visit)  //二叉树先序遍历算法（迭代版#2）
 template <typename T, typename VST> //元素类型、操作器
 void travPre_R(BinNodePosi(T) x, VST& visit) //二叉树先序遍历算法（递归版）
 {
-	if (!x) return;
+	if (!x)
+		return;
 	visit(x->data);
 	travPre_R(x->lc, visit);
 	travPre_R(x->rc, visit);
@@ -262,31 +266,97 @@ void BinNode<T>::travIn(VST& visit)  //二叉树中序遍历算法统一入口
 }
 
 template <typename T> //从当前节点出发，沿左分支不断深入，直至没有左分支的节点
-static void goAlongVine(BinNodePosi(T) x, Stack<BinNodePosi(T)>& S) 
+static void goAlongVine(BinNodePosi(T) x, Stack<BinNodePosi(T)>& S)
 {
-	while (x) 
-	{ 
-		S.push(x); 
-		x = x->lc; 
+	while (x)
+	{
+		S.push(x);
+		x = x->lc;
 	} //当前节点入栈后随即向左侧分支深入，迭代直到无左孩子
 }
 
 template <typename T, typename VST> //元素类型、操作器
 void travIn_I1(BinNodePosi(T) x, VST& visit) { //二叉树中序遍历算法（迭代版#1）
 	Stack<BinNodePosi(T)> S; //辅助栈
-	while (true) {
+	while (true)
+	{
 		goAlongVine(x, S); //从当前节点出发，逐批入栈
-		if (S.empty()) break; //直至所有节点处理完毕
-		x = S.pop(); 
+		if (S.empty())
+			break; //直至所有节点处理完毕
+		x = S.pop();
 		visit(x->data); //弹出栈顶节点并访问之
 		x = x->rc; //转向右子树
 	}
 }
 
+
+template <typename T, typename VST> //元素类型、操作器
+void travIn_I2(BinNodePosi(T) x, VST& visit) { //二叉树中序遍历算法（迭代版#2）
+	Stack<BinNodePosi(T)> S; //辅助栈
+	while (true)
+		if (x)
+		{
+			S.push(x); //根节点进栈
+			x = x->lc; //深入遍历左子树
+		}
+		else if (!S.empty())
+		{
+			x = S.pop(); //尚未访问的最低祖先节点退栈
+			visit(x->data); //访问该祖先节点
+			x = x->rc; //遍历祖先的右子树
+		}
+		else
+			break; //遍历完成
+}
+
+template <typename T, typename VST> //元素类型、操作器
+void travIn_I3(BinNodePosi(T) x, VST& visit) { //二叉树中序遍历算法（迭代版#3，无需辅助栈）
+	bool backtrack = false; //前一步是否刚从右子树回溯——省去栈，仅O(1)辅助空间
+	while (true)
+		if (!backtrack && HasLChild(*x)) //若有左子树且不是刚刚回溯，则
+			x = x->lc; //深入遍历左子树
+		else //否则——无左子树或刚刚回溯（相当于无左子树）
+		{
+			visit(x->data); //访问该节点
+			if (HasRChild(*x)) //若其右子树非空，则
+			{
+				x = x->rc; //深入右子树继续遍历
+				backtrack = false; //并关闭回溯标志
+			}
+			else //若右子树空，则
+			{
+				if (!(x = x->succ()))
+					break; //回溯（含抵达末节点时的退出返回）
+				backtrack = true; //并设置回溯标志
+			}
+		}
+}
+
+template <typename T, typename VST> //元素类型、操作器
+void travIn_I4(BinNodePosi(T) x, VST& visit) { //二叉树中序遍历（迭代版#4，无需栈或标志位）
+	while (true)
+		if (HasLChild(*x)) //若有左子树，则
+			x = x->lc; //深入遍历左子树
+		else //否则
+		{
+			visit(x->data); //访问当前节点，并
+			while (!HasRChild(*x)) //不断地在无右分支处
+			{
+				if (!(x = x->succ()))
+					return; //回溯至直接后继（在没有后继的末节点处，直接退出）
+				else
+					visit(x->data); //访问新的当前节点
+			}
+			x = x->rc; //（直至有右分支处）转向非空的右子树
+		}
+}
+
+
 template <typename T, typename VST> //元素类型、操作器
 void travIn_R(BinNodePosi(T) x, VST& visit)
 { //二叉树中序遍历算法（递归版）
-	if (!x) return;
+	if (!x)
+		return;
 	travIn_R(x->lc, visit);
 	visit(x->data);
 	travIn_R(x->rc, visit);
@@ -317,7 +387,7 @@ protected:
 public:
 	BinTree() : _size(0), _root(NULL) { } //构造函数
 
-	~BinTree() //析构函数
+	virtual ~BinTree() //析构函数
 	{
 		if (0 < _size)
 			remove(_root);
--- BinTree/queue.h
+++ BinTree/queue.h
@@ -1,4 +1,6 @@
-#pragma once
+#ifndef QUEUE_H_
+#define QUEUE_H_
+
 #include "List.h" //以List为基类
 
 template <typename T> class Queue : public List<T> { //队列模板类（继承List原有接口）
@@ -7,3 +9,6 @@ public: //size()、empty()以及其它开放接口均可直接沿用
 	T dequeue() { return remove(this->first()); } //出队：首部删除
 	T& front() { return this->first()->data; } //队首
 };
+
+
+#endif
